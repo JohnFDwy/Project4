2e5f5e018650e452319bdfb95c6a59edf0fb08f6
JohnFDwy <johnfowledwyer@gmail.com>
2024-06-06 22:21:28 -0600

    project file upload

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
-import stremlit as st
+import streamlit as st
 import pandas as pd
-import plotly.express as pt
+import plotly.express as px
 import altair as al
+
+data= pd.read_csv('vehicles_us.csv')
+
+st.header('Vehicles\' Price by Mileage')
+
+st.write(data.head())
